2aab6b6ead788a83c7abbc597dd2b66d56263e6b
M Hassan <mhassan.33431@gmail.com>
2025-11-12 15:04:38 +0500

    change in server index.js

--- server/index.js
+++ server/index.js
@@ -8,12 +8,7 @@ const app = express();
 connectToDatabase();
 
 app.use(express.json());
-app.use(
-  cors({
-    origin: "https://e-commerce-14wa.vercel.app",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-  })
-);
+app.use(cors());
 
 const port = process.env.PORT || 9000;
 
@@ -22,21 +17,21 @@ app.get("/", (req, res) => {
 });
 
 import userRoutes from "./routes/userRoutes.js";
-app.use("/api/users", userRoutes);
+app.use("api/users", userRoutes);
 import productRoutes from "./routes/productRoutes.js";
-app.use("/api/products", productRoutes);
+app.use("api/products", productRoutes);
 import cartRoutes from "./routes/cartRoutes.js";
-app.use("/api/cart", cartRoutes);
+app.use("api/cart", cartRoutes);
 import checkoutRoutes from "./routes/checkoutRoutes.js";
-app.use("/api/checkout", checkoutRoutes);
+app.use("api/checkout", checkoutRoutes);
 import orderRoutes from "./routes/orderRoutes.js";
-app.use("/api/orders", orderRoutes);
+app.use("api/orders", orderRoutes);
 import subscribeRoutes from "./routes/subscribeRoutes.js";
-app.use("/api/subscribe", subscribeRoutes);
+app.use("api/subscribe", subscribeRoutes);
 
 //Admin
 import adminRoutes from "./routes/adminRoutes.js";
-app.use("/api/admin", adminRoutes);
+app.use("api/admin", adminRoutes);
 
 app.listen(port, () => {
   console.log(`✅ Server is running on http://localhost:${port}`);
