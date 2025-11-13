96f0bf9398e31fa0f07b424d3b450dd36b710b2b
M Hassan <mhassan.33431@gmail.com>
2025-11-13 15:05:14 +0500

    change url in redux orderAction.js

--- client-ui/src/redux/actions/orderAction.js
+++ client-ui/src/redux/actions/orderAction.js
@@ -6,7 +6,7 @@ export const fetchUserOrder = createAsyncThunk(
   async (_, { rejectWithValue }) => {
     try {
         const response = await axios.get(
-        `${process.env.REACT_APP_BACKEND_URL}api/orders/my-orders`,
+        `${process.env.REACT_APP_BACKEND_URL}api/my-orders`,
         {
             headers:{
                 Authorization: `Bearer ${localStorage.getItem("userToken")}`,
